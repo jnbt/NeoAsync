84001f84da3aa59c1f29006417dfa71f10fbe8c6
Jonas Thiel <jt@neopoly.de>
2019-11-28 10:38:53 +0100

    Add AssemblyInfo

---
+++ Assets/NeoAsync/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NeoAsync")]
+[assembly: AssemblyDescription("NeoCollections is a class library for asynchronous tasks in Unity3D")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Neopoly GmbH")]
+[assembly: AssemblyProduct("NeoCollections")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("05d73af5-b21d-4012-a47d-9b59038a46d1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
